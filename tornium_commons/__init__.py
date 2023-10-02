f31c53d8908d36b216fe6ce4ab54b46a46333cb7
dssecret <webmaster@deek.sh>
2023-10-02 15:09:03 -0500

    Updated config to use Pydantic
    Signed-off-by: dssecret <webmaster@deek.sh>

--- tornium_commons/__init__.py
+++ tornium_commons/__init__.py
@@ -13,6 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .config import Config
+from .config import Settings
 from .dbucket import DBucket
 from .redisconnection import rds
